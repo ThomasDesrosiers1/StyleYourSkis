f6a6b505f072f15f9584cde28eb1b3ac4178e6b6
Thomas Desrosiers <desrosiers.thomas@outlook.com>
2023-04-08 12:22:37 -0400

    Menu couleurs vf

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,24 @@
 
 //======= MENU-DROPDOWN =======//
 
-document.querySelector(".dropdown-arrow").addEventListener("click", openMenu);
+document.querySelector(".dropdown-arrow1").addEventListener("click", openMenuCategories);
+document.querySelector(".dropdown-arrow2").addEventListener("click", openMenuCouleurs);
 
-function openMenu(){
+function openMenuCategories(){
 
   let menuCategories = document.getElementById("dropdown-categories");
-
   menuCategories.classList.toggle("visible");
-  
+   
+}
+
+function openMenuCouleurs(){
+
+  let menuCouleurs = document.getElementById("dropdown-couleurs");
+  menuCouleurs.classList.toggle("visible");
 }
 
 
-//======== FENÊTRE MODALE ========//
+//======== FENÊTRE MODALE RECHERCHE ========//
 
 const modal = document.querySelector('.modal');
 const searchLink = document.querySelector('.search');
